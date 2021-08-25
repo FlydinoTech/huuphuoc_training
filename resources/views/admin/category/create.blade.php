4bad717dc8d885065cc6309745d256c3416632f8
lapulga100911 <nguyenvanhuuphuoc17t1@gmail.com>
2021-08-25 10:45:40 +0700

    Update_admin_category_formOpen

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -15,7 +15,7 @@
                     <div class="panel-body">
                         <div class="row">
                             <div class="">
-                                {!! Form::open() !!} 
+                                {!! Form::open(['method' => 'POST', 'url' => 'route('category.store')']) !!}
                                     @include('admin.category.form')
                                     {!! Form::submit( 'Thêm', ['class' => 'btn-submit']) !!}
                                     </div>
